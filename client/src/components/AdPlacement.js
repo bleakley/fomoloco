ea32531d525bf5c5963eaacc65a1d03a52d8334b
Max Radin <radin.max@gmail.com>
2021-02-20 23:44:06 -0500

    Cleaning up ad placement

--- client/src/components/AdPlacement.js
+++ client/src/components/AdPlacement.js
@@ -1,43 +1,102 @@
 import React, { Component } from "react";
-import PaypalButton from './PaypalButton';
+import PaypalButton from "./PaypalButton";
 
-const AdaWallet = 'addr1q9sz5cunrh9r6syfwpd9h6wgv8lpd0c60n4yhetv0m9xdg0kmcxwts5fppwf8zewaw05cc3yvamwzu6ytf9q3923sm6qetwcqk';
-const DogeWallet = 'DAbzsgE1oaxLvqsrvfEA1QHTNqXVtDHhtF';
+const AdaWallet =
+  "addr1q9sz5cunrh9r6syfwpd9h6wgv8lpd0c60n4yhetv0m9xdg0kmcxwts5fppwf8zewaw05cc3yvamwzu6ytf9q3923sm6qetwcqk";
+const DogeWallet = "DAbzsgE1oaxLvqsrvfEA1QHTNqXVtDHhtF";
 
 class DonationAd extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <div>
-                <div style={{display: 'flex', justifyItems: 'center', justifyContent: 'center', marginTop: '20px'}}>🤑 Please support FOMO LOCO web hosting and development 🤑</div>
-                <div style={{display: 'flex', flexDirection: 'column'}}>
-                    <div style={{display: 'flex', justifyItems: 'center', justifyContent: 'center', marginTop: '20px'}}>
-                        <div style={{marginTop: '2px', marginRight: '15px'}}>💎PayPal💎</div>
-                        <div><PaypalButton /></div>
-                    </div>
-                    <div style={{display: 'flex', justifyItems: 'center', justifyContent: 'center', marginTop: '0px'}}>
-                        <div style={{marginTop: '18px', marginRight: '15px'}}>🚀ADA🚀</div>
-                        <div><pre style={{maxWidth: '150px', overflowX: 'scroll', marginRight: '15px'}}>{AdaWallet}</pre></div>
-                        <div style={{marginTop: '18px', marginRight: '15px'}}>📈DOGE📈</div>
-                        <div><pre style={{maxWidth: '150px', overflowX: 'scroll'}}>{DogeWallet}</pre></div>
-                    </div>
-                </div>
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div style={{display: "flex", flexDirection: "column", justifyContent: "space-between", paddingRight:"16px", paddingLeft:"16px"}}>
+        <div
+          style={{
+            display: "flex",
+            justifyItems: "center",
+            justifyContent: "center",
+            marginTop: "20px",
+          }}
+        >
+          🤑 Please support FOMO LOCO web hosting and development 🤑
+        </div>
+        <div style={{ display: "flex", flexDirection: "row" , justifyContent: "space-between", alignItems: "flex-start"}}>
+          <div
+            style={{
+              display: "flex",
+              justifyItems: "center",
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: "20px",
+              flexDirection: "column",
+            }}
+          >
+            <div style={{ marginTop: "2px", marginRight: "15px" }}>
+              💎PayPal💎
             </div>
-        )
-    }
+            <div>
+              <PaypalButton />
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyItems: "center",
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: "0px",
+              flexDirection: "column",
+            }}
+          >
+            <div style={{ marginTop: "18px", marginRight: "15px" }}>
+              🚀ADA🚀
+            </div>
+            <div>
+              <pre
+                style={{
+                  maxWidth: "150px",
+                  overflowX: "scroll",
+                  marginRight: "15px",
+                }}
+              >
+                {AdaWallet}
+              </pre>
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyItems: "center",
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: "0px",
+              flexDirection: "column",
+            }}
+          >
+            <div style={{ marginTop: "18px", marginRight: "15px" }}>
+              📈DOGE📈
+            </div>
+            <div>
+              <pre style={{ maxWidth: "150px", overflowX: "scroll" }}>
+                {DogeWallet}
+              </pre>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 class AdPlacement extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <DonationAd />
-        )
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <DonationAd />;
+  }
 }
 
 export default AdPlacement;
