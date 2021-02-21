167fc4ab02013b269741fff1458bf57dab1a4f02
Max Radin <radin.max@gmail.com>
2021-02-21 03:02:53 -0500

    Fixing starting cash

--- server/User.js
+++ server/User.js
@@ -69,7 +69,7 @@ class User {
     this.id = uuid.v4();
     this.name = `u${this.id}`;
     this.suggestedName = market.getUniqueUserName();
-    this.cash = 300;
+    this.cash = 30;
     this.shares = {};
     this.type = constants.TRADER_TYPE_PLAYER;
     this.upgrades = {
